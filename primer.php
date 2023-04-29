55a30d905db6d71aa3629862fe5334fb8cc11ab2
ValenSaZu <111878177+ValenSaZu@users.noreply.github.com>
2023-04-28 19:31:24 -0500

    Add files via upload

---
+++ primer.php
@@ -0,0 +1,31 @@
+<?php
+$color="red";
+echo "My house is ", $color, "<br>";
+//strlen tamaño del string
+//strrev reversa de la cadena
+//str_replace: $newtext= str_replace(pALABRA QUE SERA REEMPLAZADA, NUEVA PALABRA, AKGO MÁS)
+//var_dump: maybe como if
+$a = 50;
+$b = 10;
+if ($a > $b) {
+    echo "Hello world";
+}
+if ($a != $b){
+    echo "Hello wolf";
+}
+if($a==$b){
+    echo "si";
+}else{
+    echo "no";
+}
+//elseif
+//swith case default:
+//foreach
+/*foreach ($colors as $x){
+
+}*/
+//function para crear funciones
+//echo count ($variable); imprime el array
+//Array asociativa => ejmplo "Jose"=>21
+
+?>
